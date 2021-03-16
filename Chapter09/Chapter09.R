cf4d2818ee15527b41b0cec8f9aedd35176e3bf2
山路憲 <ken.yamaji@gmail.com>
2021-03-17 00:09:17 +0900

    20210316

---
+++ Chapter09/Chapter09.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(rstan)
+
+rm(list=ls(all.names=TRUE))
+
+download.file('https://kuboweb.github.io/-kubo/stat/iwanamibook/fig/gibbs/d.RData', './Chapter09/d.RData')
+load('./Chapter09/d.RData')
+
+d %>% ggplot(aes(x=x, y=y)) + geom_point()
+
+fit.glm <- glm(y~x, data=d, family=poisson(link=log))
+summary(fit.glm)
+
+d.list <- list(
+  N = nrow(d),
+  x = d$x,
+  y = d$y
+)
+
+fit.stan <- stan(
+  file = './Chapter09/model1.stan',
+  data = d.list)
+
+print(fit.stan)
+plot(fit.stan)
+pairs(fit.stan, pars=c('beta1', 'beta2'))
